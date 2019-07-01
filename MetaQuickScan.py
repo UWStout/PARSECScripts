7260f9d76a13a09c4b8e58f2a690f761c1c097a5
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-07-01 12:51:48 -0500

    Added all the steps relevent for quick (under 15min) results for a preview of scan data.

--- MetaQuickScan.py
+++ MetaQuickScan.py
@@ -1,8 +1,11 @@
 """A script for a quick, "dirty" Metashape scan"""
 
-PATH_TO_IMAGES = "D:/OneDrive/Career/jobs/artDesign/2019/parsec/Data/Sim/Eric/EricMarkLowQ/"
+#AIW Change this path to where user has their scan data stored. Create a folder 
+# in this dir named 'masks' and place empty images to use for bg masks.
+PATH_TO_IMAGES = "D:/OneDrive/Career/jobs/artDesign/2019/parsec/Data/Sim/Eric/EricMarkersHQ/"
 IMAGE_PREFIX = "EricMarkers"
-PATH_TO_MASKS = "D:/OneDrive/Career/jobs/artDesign/2019/parsec/Data/Sim/Eric/EricMarkLowQ/masks/{filename}_mask.tif"
+#AIW Change this path to the masks' folder in the user's scan data directory.
+PATH_TO_MASKS = "D:/OneDrive/Career/jobs/artDesign/2019/parsec/Data/Sim/Eric/EricMarkersHQ/masks/{filename}_mask.tif"
 PHASE_LABEL = "none"
 
 import Metashape
@@ -72,32 +75,67 @@ doc.save()
 #AIW Getting reference to camera. Index is out of range if not run after chunk.addPhotos.
 camera = chunk.cameras[0]
 
-#AIW From API "Import masks for multiple cameras." Import background images for masking out the background. Camera must be referenced for this step to work.
+#AIW From API "Import masks for multiple cameras." 
+# - Import background images for masking out the background. 
+# - Camera must be referenced for this step to work.
 phaseTime = time.time()
 PHASE_LABEL = "Masking Photos"
 chunk.importMasks(path=PATH_TO_MASKS, source=Metashape.MaskSourceBackground, operation=Metashape.MaskOperationReplacement, tolerance=10, progress=progress_callback)
 print_time_elapsed(phaseTime)
 doc.save()
 
-#AIW From API "Create markers from coded targets." Detects markers with default settings.
+#AIW From API "Create markers from coded targets." 
+# - Detects markers with default settings.
 phaseTime = time.time()
 PHASE_LABEL = "Detecting Markers"
 chunk.detectMarkers(tolerance=50, filter_mask=False, inverted=False, noparity=False, maximum_residual=5, progress=progress_callback)
 print_time_elapsed(phaseTime)
 doc.save()
 
-"""AIW From API "Perform image matching for the chunk frame." First step of the "Workflow" process called "Align Photos" in Metashape GUI, 
-which generates the Sparse Cloud. Keypoint and Tiepoint of 0 is = no restriction. Accuracy below MediumAccuracy consistently results in failed camera alignment"""
+#AIW From API "Perform image matching for the chunk frame." 
+# - First step of the Metashape GUI "Workflow" process called "Align Photos", which generates the Sparse Cloud/Tie Points. 
+# - Keypoints and Tiepoints for this script is Agisoft's suggested default. 
+# - Accuracy below MediumAccuracy consistently results in failed camera alignment.
+# - HighAccuracy is used in this script as the results are better for and the time added is negligible.
 phaseTime = time.time()
 PHASE_LABEL = "Matching Photos"
-chunk.matchPhotos(accuracy=Metashape.MediumAccuracy, reference_preselection=False, filter_mask=True, mask_tiepoints=False, keypoint_limit=(0), tiepoint_limit=(0), progress=progress_callback)
+chunk.matchPhotos(accuracy=Metashape.HighAccuracy, generic_preselection=True, filter_mask=True, mask_tiepoints=False, keypoint_limit=(40000), tiepoint_limit=(4000), progress=progress_callback)
 print_time_elapsed(phaseTime)
 doc.save()
 
-#AIW From API "Perform photo alignment for the chunk." Second step of the "Workflow" process called "Align Photos" in Metashape GUI, which generates the Sparse Cloud.
+#AIW From API "Perform photo alignment for the chunk." 
+# - Second step of the Metashape GUI "Workflow" process called "Align Photos", which generates the Sparse Cloud/Tie Points.
 phaseTime = time.time()
 PHASE_LABEL = "Aligning Cameras"
 chunk.alignCameras(progress=progress_callback)
+print_time_elapsed(phaseTime)
+doc.save()
+
+#SFB Changes the dimensions of the chunk's reconstruction volume.
+NEW_REGION = doc.chunk.region
+NEW_REGION.size = NEW_REGION.size * 2.0
+doc.chunk.region = NEW_REGION
+doc.save()
+
+#AIW From API "Generate model for the chunk frame." Builds mesh to be used in the last steps.
+phaseTime = time.time()
+PHASE_LABEL = "3D Model"
+chunk.buildModel(surface=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation, face_count=Metashape.HighFaceCount, source=Metashape.PointCloudData, vertex_colors=True, progress=progress_callback)
+print_time_elapsed(phaseTime)
+doc.save()
+
+#AIW From API "Generate uv mapping for the model."
+phaseTime = time.time()
+PHASE_LABEL = "UV 3D Model"
+chunk.buildUV(adaptive_resolution=True, progress=progress_callback)
+print_time_elapsed(phaseTime)
+doc.save()
+
+#AIW From API "Generate texture for the chunk." 
+# - Generates a basic texture for the 3D model.
+phaseTime = time.time()
+PHASE_LABEL = "Generate Texture"
+chunk.buildTexture(blending=Metashape.MosaicBlending, size=(1024), fill_holes=False, progress=progress_callback)
 print_time_elapsed(phaseTime)
 doc.save()
 

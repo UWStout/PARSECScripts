1848281d3970b320dc3093aa478fa7c68da3e8a5
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-07-20 18:19:07 -0500

    Cleaned up script.

--- MetaQuick.py
+++ MetaQuick.py
@@ -8,36 +8,23 @@ import MetaUtils
 
 #AIW Change this path to where user has their scan data stored. Create a folder 
 # in this dir named 'masks' and place empty images to use for bg masks.
-PATH_TO_IMAGES = "C:/SimulatedScans/PythonTest-MS/"
+PATH_TO_IMAGES = "E:/ParsecExp/EricMarkersHQ/"
 IMAGE_PREFIX = "EricMarkers"
 #AIW Change this path to the masks' folder in the user's scan data directory.
-PATH_TO_MASKS = "C:/SimulatedScans/PythonTest-MS/Masks/{filename}_mask.tif"
+PATH_TO_MASKS = "E:/ParsecExp/EricMarkersHQ/Masks/{filename}_mask.tif"
 PHASE_LABEL = "none"
 
 #AIW Creates a log.
 MetaUtils.log(PATH_TO_IMAGES, IMAGE_PREFIX)
-
-#SFB Erase the current line by printing spaces
-# - Does not advance to the next line
-def blank_line(length=80):
-    empty = " " * length
-    print(empty, end='\r')
-
-#SFB Print the current progress over the current line
-# - Does not advance to the next line
-def progress_callback(prog):
-    blank_line()
-    print("%s: Progress %6.2f%%" %(PHASE_LABEL, prog), end='\r')
-    sys.stdout.flush()
-
-def print_time_elapsed(startTime):
-    print("\nElapsed Time: %.2fsecs" %(time.time() - startTime))
 
 #AIW Check compatibility.
 MetaUtils.compat(Metashape.app.version)
 
 #AIW Enables GPU processing in Metashape.
 MetaUtils.use_gpu()
+
+#AIW creates a new document or loads a previous one.
+#MetaUtils.doc_mngr(PATH_TO_IMAGES, IMAGE_PREFIX)
 
 #SFB Get reference to the currently active DOM
 doc = Metashape.Document()
@@ -55,48 +42,24 @@ except:
     doc.save("{}{}.psx" .format(PATH_TO_IMAGES, IMAGE_PREFIX))
     chunk = doc.addChunk()
 
-#SFB Indicate processing is starting
-sys.stdout.flush()
-print("Starting processing:")
-start = time.time()
-
 #AIW Creates an image list and adds them to the current chunk.
 MetaUtils.image_list(chunk, PATH_TO_IMAGES, IMAGE_PREFIX)
 
 #AIW Creates masks.
-#MetaUtils.auto_mask(chunk, PATH_TO_MASKS)
+MetaUtils.auto_mask(chunk, PATH_TO_MASKS)
 
 #AIW Aligns photos.
-#MetaWork.quick_align(chunk)
+MetaWork.quick_align(chunk)
 
 #AIW Corrects the chunk.
-#MetaUtils.chunk_correct(doc, chunk)
+MetaUtils.chunk_correct(doc, chunk)
 
 #AIW Creats a quick model.
-#MetaWork.quick_model(chunk)
+MetaWork.quick_model(chunk)
 
-"""#AIW From API "Generate model for the chunk frame." Builds mesh to be used in the last steps.
-phaseTime = time.time()
-PHASE_LABEL = "3D Model"
-chunk.buildModel(surface=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation, face_count=Metashape.HighFaceCount, source=Metashape.PointCloudData, vertex_colors=True, progress=progress_callback)
-print_time_elapsed(phaseTime)
-
-#AIW From API "Generate uv mapping for the model."
-phaseTime = time.time()
-PHASE_LABEL = "UV 3D Model"
-chunk.buildUV(adaptive_resolution=True, progress=progress_callback)
-print_time_elapsed(phaseTime)
-
-#AIW From API "Generate texture for the chunk." 
-# - Generates a basic texture for the 3D model.
-phaseTime = time.time()
-PHASE_LABEL = "Generate Texture"
-chunk.buildTexture(blending=Metashape.MosaicBlending, size=(1024), fill_holes=False, progress=progress_callback)
-print_time_elapsed(phaseTime)"""
 doc.save()
 
 print("Done")
-print_time_elapsed(start)
 
 #AIW Exits script and releases the lock on the current document.
 sys.exit()

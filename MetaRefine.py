0686979d402b20c59d75212a32dd797690f899bf
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-07-20 20:41:41 -0500

    Testing refinement code

--- MetaRefine.py
+++ MetaRefine.py
@@ -17,24 +17,8 @@ PHASE_LABEL = "none"
 #AIW Creates a log.
 MetaUtils.log(PATH_TO_IMAGES, IMAGE_PREFIX)
 
-#SFB Erase the current line by printing spaces
-# - Does not advance to the next line
-def blank_line(length=80):
-    empty = " " * length
-    print(empty, end='\r')
-
-#SFB Print the current progress over the current line
-# - Does not advance to the next line
-def progress_callback(prog):
-    blank_line()
-    print("%s: Progress %6.2f%%" %(PHASE_LABEL, prog), end='\r')
-    sys.stdout.flush()
-
-def print_time_elapsed(startTime):
-    print("\nElapsed Time: %.2fsecs" %(time.time() - startTime))
-
 #AIW Check compatibility.
-MetaUtils.compat(Metashape.app.version)
+MetaUtils.check_ver(Metashape.app.version)
 
 #AIW Enables GPU processing in Metashape.
 MetaUtils.use_gpu()
@@ -55,12 +39,8 @@ except:
     doc.save("{}{}.psx" .format(PATH_TO_IMAGES, IMAGE_PREFIX))
     chunk = doc.addChunk()
 
-#SFB Indicate processing is starting
-sys.stdout.flush()
-print("\nStarting processing:")
-start = time.time()
-
-MetaWork.genUse_dense_cloud(chunk)
+#AIW Creates a dense cloud for general use.
+MetaWork.gen_dense_cloud(chunk)
 
 """#AIW From API "Generate depth maps for the chunk."
 # - First step of the Metashape GUI "Workflow" process called "Dense Cloud".
@@ -78,6 +58,9 @@ PHASE_LABEL = "Building Dense Cloud"
 chunk.buildDenseCloud(point_colors=True, keep_depth=True, max_neighbors=100, progress=progress_callback)
 print_time_elapsed(phaseTime)
 doc.save()"""
+
+#AIW Creates a modelfor general use.
+MetaWork.gen_model(chunk)
 
 """#AIW From API "Generate model for the chunk frame." 
 # - Builds mesh to be used for next steps.
@@ -103,7 +86,6 @@ print_time_elapsed(phaseTime)
 doc.save()"""
 
 print("Done")
-print_time_elapsed(start)
 
 #AIW Exits script and releases the lock on the current document.
 sys.exit()

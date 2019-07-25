f9c997a38b79510689fcbb7abc2e40bf3eabcc7b
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-07-25 13:36:58 -0500

    Testing reading from an existing file

--- argparsetest.py
+++ argparsetest.py
@@ -1,14 +1,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Quickly Process Photogrammetry Images.')
-parser.add_argument('-I', '-images', metavar='ImagePath', required=True,
-                    help='Path to the folder containing subject images.')
-parser.add_argument('-M', '-masks', metavar='MasksPath', required=True,
-                    help='Path to the folder containing images for background subtraction.')
-parser.add_argument('-N', '-name', metavar='NamePrefix', default='MetaPy',
-                    help='A prefix to apply to log and MetaShape file names.')
+#parser.add_argument('-I', '-images', metavar='ImagePath', required=True,
+                    #help='Path to the folder containing subject images.')
+#parser.add_argument('-M', '-masks', metavar='MasksPath', required=True,
+                    #help='Path to the folder containing images for background subtraction.')
+#parser.add_argument('-N', '-name', metavar='NamePrefix', default='MetaPy',
+                    #help='A prefix to apply to log and MetaShape file names.')
+parser.add_argument('F', type=argparse.FileType(), help='Access a file.' )
 
-args = parser.parse_args()
-print(args.N)
-print(args.I)
-print(args.M)
+args = parser.parse_args(['test.txt'])
+print(args.F.read())
+#print(args.N)
+#print(args.I)
+#print(args.M)

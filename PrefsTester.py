8dccc9365a9ae84e734b227f302740c0a40e4489
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-08-03 14:47:28 -0500

    Now uses prefs from ProjectPrefs

--- PrefsTester.py
+++ PrefsTester.py
@@ -1,6 +1,4 @@
 from ProjectPrefs import ProjectPrefs
 
 prefs = ProjectPrefs()
-prefs.setPref('MaskPath', '/my/crazy/dir')
-prefs.saveConfig('result.ini')
-print(prefs.getPref(prefName='MaskPath'))
+prefs.saveConfig('wahleraPrefs.ini')

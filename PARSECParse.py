41cdab75e7f4bdac02bd9f150a04f997482ab554
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-08-11 00:12:48 -0500

    Testing argparse functions

--- PARSECParse.py
+++ PARSECParse.py
@@ -3,9 +3,47 @@ import argparse
 
 from ProjectPrefs import ProjectPrefs
 
-PATH_TO_IMAGES = "E:/ParsecExp/EricMarkersHQ/"
-IMAGE_PREFIX = "EricMarkers"
-PATH_TO_MASKS = "E:/ParsecExp/EricMArkersHQ/Masks/{filename}_mask.tif"
+PATH_TO_IMAGES = ""
+IMAGE_PREFIX = ""
+PATH_TO_MASKS = ""
+
+parser = argparse.ArgumentParser(description='Program for processing photogrammetry data with Metashape')
+
+#AIW Sets project
+#AIW parser.add_argument('-P', '--project', help='Specifies project file path and name.')
+#AIW Runs MetaQuickClass
+parser.add_argument('-P', '--project', choices=['load', 'new'], required=True, help='')
+parser.add_argument('-Q', '--quick', action="store_true", help='Quickly process photogrammetry data')
+parser.add_argument('-R', '--refine', action="store_true", help='Refines previously processed photogrammetry data')
+
+args = parser.parse_args()
+if args.project[0]:
+    prefs = input("Where would you like to load project from? ") + input("\nPlease enter project name: ") + (".ini")
+
+    prefs = ProjectPrefs()
+
+    PATH_TO_IMAGES = prefs.getPref(prefName='ImagePath')
+    print(PATH_TO_IMAGES)
+    IMAGE_PREFIX = prefs.getPref(prefName='NamePrefix')
+    print(IMAGE_PREFIX)
+    PATH_TO_MASKS = prefs.getPref(prefName='MaskPath',)
+    print(PATH_TO_MASKS)
+
+elif args.project[1]:
+    PATH_TO_IMAGES = input("Please enter the path to images: ")
+    IMAGE_PREFIX = input("Please enter filename prefix: ")
+    PATH_TO_MASKS = input("Please enter file path and format for background images: ")
+
+    prefs = ProjectPrefs()
+
+    PATH_TO_IMAGES = prefs.getPref(prefName='ImagePath')
+    print(PATH_TO_IMAGES)
+    IMAGE_PREFIX = prefs.getPref(prefName='NamePrefix')
+    print(IMAGE_PREFIX)
+    PATH_TO_MASKS = prefs.getPref(prefName='MaskPath',)
+    print(PATH_TO_MASKS)
+
+    prefs.saveConfig(IMAGE_PREFIX)
 
 #SFB Import and initialize the logging system
 #SFB This also redirects all MetaScan output
@@ -18,22 +56,14 @@ import Metashape
 import MetaWork
 from MetaUtilsClass import MetaUtils
 
-parser = argparse.ArgumentParser()
-#AIW Sets project
-#AIW parser.add_argument('-P', '--project', help='Specifies project file path and name.')
-#AIW Runs MetaQuickClass
-parser.add_argument('-Q', '--quick', type=MetaWork.metaQuick, help='Quickly process photogrammetry data')
-parser.add_argument('-R', '--refine', help='Refines previously processed photogrammetry data')
-
 MetaUtils.CHECK_VER(Metashape.app.version)
 MetaUtils.USE_GPU()
 
-args = parser.parse_args()
-#args.quick = MetaWork.metaQuick
-#args.quick(PATH_TO_IMAGES, IMAGE_PREFIX, PATH_TO_MASKS)
+if args.quick:
+    MetaWork.metaQuick(PATH_TO_IMAGES, IMAGE_PREFIX, PATH_TO_MASKS)
 
+if args.refine:
+    MetaWork.metaRefine(PATH_TO_IMAGES, IMAGE_PREFIX, PATH_TO_MASKS)
 
-#args.refine = MetaWork.metaRefine
-#args.refine(PATH_TO_IMAGES, IMAGE_PREFIX)
-
-#prefs = ProjectPrefs(args.project)
+if args.project:
+    prefs = ProjectPrefs()

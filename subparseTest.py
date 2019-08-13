75f61b5d801907506218126748bad161f63c028a
DarkCorvid <wahlera9317@my.uwstout.edu>
2019-08-13 12:38:15 -0500

    Experimenting with sub parse

--- subparseTest.py
+++ subparseTest.py
@@ -1,25 +1,33 @@
 import argparse
 
+#AIW Global variables
+PATH_TO_PROJECT = "Placeholder"
+PATH_TO_IMAGES = "Placeholder"
+IMAGE_PREFIX = "Placeholder"
+PATH_TO_MASKS = "Placeholder"
+
 #AIW Main parser
 parser = argparse.ArgumentParser(prog='subparseTest', description='Testing subparser')
-parser.add_argument('-P', '--project', help='Select project')
-subparsers = parser.add_subparsers(help='Options for selected project')
+subparsers = parser.add_subparsers(help='PARSECParse options')
 
-#AIW Parser for loading existing project.ini
-parser_l = subparsers.add_parser('-l', help='Load help')
-parser_l.add_argument('--load', action='store_true', help='Loads project from specified filepath')
+#AIW Subparser for project options
+project_parser = subparsers.add_parser('project', help='Project options')
+project_parser.add_argument('--path', action='store', help='Specify project path')
+project_parser.add_argument('--images', action='store', help='Path to the folder containing subject images.')
+project_parser.add_argument('--name', action='store', help='A prefix to apply to log and MetaShape file names.')
+project_parser.add_argument('--masks', action='store', help='Path to the images for background subtraction with filename pattern.')
+project_parser.add_argument('--load', action='store_true', help='Loads project from specified filepath')
+project_parser.add_argument('--new', action='store_true', help='Saves project in location specified by filepath')
 
-#AIW Parser for creating a new project.ini
-parser_n = subparsers.add_parser('-n', help='New help')
-parser_n.add_argument('--new', action='store_true', help='Saves project in specified location')
-parser_n.add_argument('--images', help='Path to the folder containing subject images.')
-parser_n.add_argument('--masks', help='Path to the images for background subtraction with filename pattern.')
-parser_n.add_argument('--name', help='A prefix to apply to log and MetaShape file names.')
+#AIW Subparser for project options
+workflow_parser = subparsers.add_parser('workflow', help='Workflow options')
+workflow_parser.add_argument('--quick', action='store', help='Quick photogrammetry processing')
+workflow_parser.add_argument('--refine', action='store_true', help='Refinement of quickly processed photogrammetry data.')
 
 args = parser.parse_args()
 
-if args.project == 'n':
-    print('Creating new project')
+#AIW Changes global variables to parsed user input
+if args.project == '--path':
+    PATH_TO_PROJECT = args.project.path
 
-if args.project == 'l':
-    print('Loading existing project')
+print(PATH_TO_PROJECT)
